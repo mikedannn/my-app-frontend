eb4a7fb9b89d66396dae9c5c846b5faf2cff8647
Michael <michael.daniels1013@gmail.com>
2022-11-06 20:52:46 -0600

    Added all necessary files to front end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from './Home';
 import NavBar from './NavBar';
+import UserContainer from './UserContainer';
 import React, { useEffect, useState } from 'react';
 import About from './About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
